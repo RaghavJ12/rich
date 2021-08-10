3e65064a910b6b87cd10da36e0049bb483b07863
RaghavJ12 <50922572+RaghavJ12@users.noreply.github.com>
2021-08-10 17:59:01 +0530

    class

--- components/dashside.js
+++ components/dashside.js
@@ -15,24 +15,24 @@ export default function Dashside() {
         return (
             <>
                 <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-                <div class="card has-text-centered" id="s2">
-                    <div class="sidex py-4"><img src="/images/rich_logo.png" style={{ height: '40px' }} /></div>
+                <div className="card has-text-centered" id="s2">
+                    <div className="sidex py-4"><img src="/images/rich_logo.png" style={{ height: '40px' }} /></div>
                     <Link href="./inbox">
                         <a>
-                            <div class="act py-5"><i class="fa fa-inbox icon is-medium" aria-hidden="true"></i></div>
+                            <div className="act py-5"><i className="fa fa-inbox icon is-medium" aria-hidden="true"></i></div>
                         </a>
                     </Link>
                     <Link href="./friends">
                         <a>
-                            <div class="side py-5"><i class="fa fa-user-friends icon is-medium" aria-hidden="true"></i></div>
+                            <div className="side py-5"><i className="fa fa-user-friends icon is-medium" aria-hidden="true"></i></div>
                         </a>
                     </Link>
                     <Link href="./analytics">
                         <a>
-                            <div class="side py-5"><i class="fa fa-chart-line icon is-medium" aria-hidden="true"></i></div>
+                            <div className="side py-5"><i className="fa fa-chart-line icon is-medium" aria-hidden="true"></i></div>
                         </a>
                     </Link>
-                    <div class="us py-5"><img src='./images/user3.jpeg' class="cp ml-2" /><i class="fas fa-circle icon ind pt-1" aria-hidden="true"></i></div>
+                    <div className="us py-5"><img src='./images/user3.jpeg' className="cp ml-2" /><i className="fas fa-circle icon ind pt-1" aria-hidden="true"></i></div>
                 </div>
             </>
         )
@@ -41,24 +41,24 @@ export default function Dashside() {
         return (
             <>
                 <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-                <div class="card has-text-centered" id="s2">
-                    <div class="sidex py-5"><img src="/images/rich_logo.png" style={{ height: '40px' }} /></div>
+                <div className="card has-text-centered" id="s2">
+                    <div className="sidex py-5"><img src="/images/rich_logo.png" style={{ height: '40px' }} /></div>
                     <Link href="./inbox">
                         <a>
-                            <div class="side py-5"><i class="fa fa-inbox icon is-medium" aria-hidden="true"></i></div>
+                            <div className="side py-5"><i className="fa fa-inbox icon is-medium" aria-hidden="true"></i></div>
                         </a>
                     </Link>
                     <Link href="./friends">
                         <a>
-                            <div class="act py-5"><i class="fa fa-user-friends icon is-medium" aria-hidden="true"></i></div>
+                            <div className="act py-5"><i className="fa fa-user-friends icon is-medium" aria-hidden="true"></i></div>
                         </a>
                     </Link>
                     <Link href="./analytics">
                         <a>
-                            <div class="side py-5"><i class="fa fa-chart-line icon is-medium" aria-hidden="true"></i></div>
+                            <div className="side py-5"><i className="fa fa-chart-line icon is-medium" aria-hidden="true"></i></div>
                         </a>
                     </Link>
-                    <div class="side py-5"><i class="fas fa-user-circle icon is-medium" aria-hidden="true"></i></div>
+                    <div className="side py-5"><i className="fas fa-user-circle icon is-medium" aria-hidden="true"></i></div>
                 </div>
             </>
         )
@@ -67,24 +67,24 @@ export default function Dashside() {
         return (
             <>
                 <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-                <div class="card has-text-centered" id="s2">
-                    <div class="sidex py-5"><img src="/images/rich_logo.png" style={{ height: '40px' }} /></div>
+                <div className="card has-text-centered" id="s2">
+                    <div className="sidex py-5"><img src="/images/rich_logo.png" style={{ height: '40px' }} /></div>
                     <Link href="./inbox">
                         <a>
-                            <div class="side py-5"><i class="fa fa-inbox icon is-medium" aria-hidden="true"></i></div>
+                            <div className="side py-5"><i className="fa fa-inbox icon is-medium" aria-hidden="true"></i></div>
                         </a>
                     </Link>
                     <Link href="./friends">
                         <a>
-                            <div class="side py-5"><i class="fa fa-user-friends icon is-medium" aria-hidden="true"></i></div>
+                            <div className="side py-5"><i className="fa fa-user-friends icon is-medium" aria-hidden="true"></i></div>
                         </a>
                     </Link>
                     <Link href="./analytics">
                         <a>
-                            <div class="act py-5"><i class="fa fa-chart-line icon is-medium" aria-hidden="true"></i></div>
+                            <div className="act py-5"><i className="fa fa-chart-line icon is-medium" aria-hidden="true"></i></div>
                         </a>
                     </Link>
-                    <div class="side py-5"><i class="fas fa-user-circle icon is-medium" aria-hidden="true"></i></div>
+                    <div className="side py-5"><i className="fas fa-user-circle icon is-medium" aria-hidden="true"></i></div>
                 </div>
             </>
         )
