d011a48e3f121d3b95b42b80943980afa10a6d31
RaghavJ12 <50922572+RaghavJ12@users.noreply.github.com>
2021-08-14 03:59:06 +0530

    log

---
+++ components/Login.js
@@ -0,0 +1,66 @@
+import { useRouter } from 'next/router'
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import Image from 'next/image'
+
+export default function Login() {
+    
+    // const [exp,setexp]=useState(null);
+    // const router = useRouter()
+    // const authenticate = event => {
+    //   const n = event.target.name.value;
+    //   const e = event.target.email.value;
+    //   const p= event.target.ph.value;
+    //   const obj={
+    //     uname: n,
+    //     email: e,
+    //     phone: p
+    //   };
+    //   setexp(obj);
+    //   router.push('/inbox')
+    //   event.preventDefault()
+    // }
+  
+    // console.log(exp);
+  
+    // const [ session, loading ] = useSession()
+  
+    return (
+      <div className="wel" style={{ backgroundColor: '#004e96' }}>
+        <br />
+        <br />
+        <div className="columns">
+          <div className="column is-4 is-offset-4">
+            <p className="is-size-1 has-text-centered ht">Welcome to FB Helper</p>
+            <br />
+            <br />
+            <div className="box">
+              <form>
+              <p className="is-size-5 has-text-weight-semibold">Enter Name: <span style={{color: "red"}}>*</span></p>
+                <br />
+                <input id="name" className="input is-primary" type="text" autoComplete="name" required placeholder="Enter Name" />
+                <br />
+                <br />
+                <p className="is-size-5 has-text-weight-semibold">Enter Email: <span style={{color: "red"}}>*</span></p>
+                <br />
+                <input id="email" className="input is-primary" type="text" autoComplete="email" required placeholder="Enter Email" />
+                <br />
+                <br/>
+                <p className="is-size-5 has-text-weight-semibold">Enter Phone: </p>
+                <br />
+                <input id="ph" className="input is-primary" type="text" autoComplete="phone" placeholder="Enter Phone" />
+                <br />
+                <br />
+                <div className="columns has-text-right">
+                  <div className="column is-4 is-offset-8">
+                    <button className="button is-rounded" id="b1" type="submit">Submit</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,26 @@
-import { useRouter } from 'next/router'
-import React, { useState, useEffect } from 'react';
-// import { useSession, getSession } from 'next-auth/client'
-// import useUser from '../lib/useUser'
-import Link from 'next/link';
-import Image from 'next/image'
-import main from '../public/images/rich_inv_logo.png'
-import { red } from '@material-ui/core/colors';
+import Head from 'next/head'
+import React from 'react';
+import { getSession } from 'next-auth/client';
+import Login from '../components/Login';
 
-export default function Home() {
-  
-  const [exp,setexp]=useState(null);
-  const router = useRouter()
-  const authenticate = event => {
-    const n = event.target.name.value;
-    const e = event.target.email.value;
-    const p= event.target.ph.value;
-    const obj={
-      uname: n,
-      email: e,
-      phone: p
-    };
-    setexp(obj);
-    router.push('/inbox')
-    event.preventDefault()
-  }
+export default function Home({session}) {
 
-  // console.log(exp);
-
-  // const [ session, loading ] = useSession()
+  if(!session)
+  return <Login />;
 
   return (
-    <div className="wel" style={{ backgroundColor: '#004e96' }}>
-      <br />
-      <br />
-      <div className="columns">
-        <div className="column is-4 is-offset-4">
-          <p className="is-size-1 has-text-centered ht">Welcome to FB Helper</p>
-          <br />
-          <br />
-          <div className="box">
-            <form onSubmit={authenticate}>
-            <p className="is-size-5 has-text-weight-semibold">Enter Name: <span style={{color: "red"}}>*</span></p>
-              <br />
-              <input id="name" className="input is-primary" type="text" autoComplete="name" required placeholder="Enter Name" />
-              <br />
-              <br />
-              <p className="is-size-5 has-text-weight-semibold">Enter Email: <span style={{color: "red"}}>*</span></p>
-              <br />
-              <input id="email" className="input is-primary" type="text" autoComplete="email" required placeholder="Enter Email" />
-              <br />
-              <br/>
-              <p className="is-size-5 has-text-weight-semibold">Enter Phone: </p>
-              <br />
-              <input id="ph" className="input is-primary" type="text" autoComplete="phone" placeholder="Enter Phone" />
-              <br />
-              <br />
-              <div className="columns has-text-right">
-                <div className="column is-4 is-offset-8">
-                  <button className="button is-rounded" id="b1" type="submit">Submit</button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-      {/* <Link href={requestUri}><a>
-        <button className="is-size-5">Login with FB</button>
-      </a>
-      </Link> */}
-    </div>
+    <Head>
+      <title>Welcome</title>
+    </Head>
   )
+}
+
+export async function getServerSideProps(context){
+  const session=await getSession(context);
+
+  return{
+    props:{
+      session
+    }
+  }
 }
