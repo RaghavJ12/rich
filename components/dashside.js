1cdd65d73651bd2365488011e6520acc6cddbfbc
RaghavJ12 <50922572+RaghavJ12@users.noreply.github.com>
2021-08-14 15:32:48 +0530

    login work

--- components/dashside.js
+++ components/dashside.js
@@ -10,36 +10,10 @@ export default function Dashside() {
     const l = router.pathname;
 
 
-    const l1 = "/inbox";
+    // const l1 = "/inbox";
     const l2 = "/friends";
     const l3 = "/analytics";
 
-    if (l.startsWith(l1)) {
-        return (
-            <>
-                <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-                <div className="card has-text-centered" id="s2">
-                    <div className="sidex py-4"><Image src={logo} height={45} width={50} /></div>
-                    <Link href="./inbox">
-                        <a>
-                            <div className="act py-5"><i className="fa fa-inbox icon is-medium" aria-hidden="true"></i></div>
-                        </a>
-                    </Link>
-                    <Link href="./friends">
-                        <a>
-                            <div className="side py-5"><i className="fa fa-user-friends icon is-medium" aria-hidden="true"></i></div>
-                        </a>
-                    </Link>
-                    <Link href="./analytics">
-                        <a>
-                            <div className="side py-5"><i className="fa fa-chart-line icon is-medium" aria-hidden="true"></i></div>
-                        </a>
-                    </Link>
-                    <div className="us py-5"><Image src={user3pic} className="cp" alt="user3" width={50} height={50}/><i className="fas fa-circle icon ind pt-1" aria-hidden="true"></i></div>
-                </div>
-            </>
-        )
-    }
     if (l.startsWith(l2)) {
         return (
             <>
@@ -92,4 +66,31 @@ export default function Dashside() {
             </>
         )
     }
+    else {
+        return (
+            <>
+                <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+                <div className="card has-text-centered" id="s2">
+                    <div className="sidex py-4"><Image src={logo} height={45} width={50} /></div>
+                    <Link href="./inbox">
+                        <a>
+                            <div className="act py-5"><i className="fa fa-inbox icon is-medium" aria-hidden="true"></i></div>
+                        </a>
+                    </Link>
+                    <Link href="./friends">
+                        <a>
+                            <div className="side py-5"><i className="fa fa-user-friends icon is-medium" aria-hidden="true"></i></div>
+                        </a>
+                    </Link>
+                    <Link href="./analytics">
+                        <a>
+                            <div className="side py-5"><i className="fa fa-chart-line icon is-medium" aria-hidden="true"></i></div>
+                        </a>
+                    </Link>
+                    <div className="us py-5"><Image src={user3pic} className="cp" alt="user3" width={50} height={50}/><i className="fas fa-circle icon ind pt-1" aria-hidden="true"></i></div>
+                </div>
+            </>
+        )
+    }
+    
 }
