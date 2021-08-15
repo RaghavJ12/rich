0040f05bd521f919097b0b0c9611bdcca921f9e1
RaghavJ12 <50922572+RaghavJ12@users.noreply.github.com>
2021-08-16 03:41:52 +0530

    modal

--- components/dashside.js
+++ components/dashside.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image'
 import logo from '../public/images/rich_logo.png'
 import { useSession, signOut } from 'next-auth/client';
-import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
+import { Modal, ModalBody, ModalFooter } from "reactstrap";
 
 export default function Dashside() {
     const router = useRouter()
@@ -116,35 +116,58 @@ export default function Dashside() {
                             width={50}
                             height={50} />
                     </div>
-                    <Modal toggle={() => setModalOpen(!modalOpen)} isOpen={modalOpen}>
-                        <div className=" modal-header">
-                            <h5 className=" modal-title" id="exampleModalLabel">
-                                Modal title
-                            </h5>
-                            <button
-                                aria-label="Close"
-                                className=" close"
-                                type="button"
-                                onClick={() => setModalOpen(!modalOpen)}
-                            >
-                                <span aria-hidden={true}>×</span>
-                            </button>
-                        </div>
-                        <ModalBody>You want to Log Out?</ModalBody>
-                        <ModalFooter>
-                            <Button
-                                color="secondary"
-                                type="button"
-                                onClick={() => setModalOpen(!modalOpen)}
-                            >
-                                No
-                            </Button>
-                            <Button color="primary" type="button" onClick={signOut}>
-                                Yes
-                            </Button>
-                        </ModalFooter>
-                    </Modal>
                 </div>
+                <Modal toggle={() => setModalOpen(!modalOpen)} isOpen={modalOpen} className="mr">
+                        <div className="card p-3 mrend" style={{ width: '60%'}}>
+                            <div className="modal-header">
+                                <div className="modtop columns">
+                                    <div className="column is-1 is-offset-11">
+                                        <div
+                                            aria-label="Close"
+                                            className="close"
+                                            onClick={() => setModalOpen(!modalOpen)}
+                                        >
+                                            <span aria-hidden={true} style={{ cursor: 'pointer' }}><i class="fas fa-times"></i></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="columns is-mobile is-multiline is-gapless">
+                                    <div className="column is-4 is-offset-4">
+                                        <div class="gradient-avatar">
+                                            <Image class="gradient-avatar__image" src={MyImage} width={130} height={130} /></div>
+                                    </div>
+                                    <div className="column is-6 is-offset-3 has-text-centered">
+                                        <p className="has-text-weight-semibold is-size-5 py-2">{sess.user.name}</p>
+                                    </div>
+                                    <div className="column is-6 is-offset-3 has-text-centered"><p className="is-size-6" style={{ color: 'green' }}><i className="fas fa-circle icon is-small pr-2"></i>Online</p></div>
+                                </div>
+                            </div>
+                            <br />
+                            <ModalBody>
+                                <p className="has-text-weight-semibold is-size-5 has-text-centered">You want to Log Out?</p>
+                            </ModalBody>
+                            <br />
+                            <ModalFooter>
+                                <div className="columns is-mobile">
+                                    <div className="column is-6 has-text-centered">
+                                        <button
+                                            className="button is-medium"
+                                            style={{minWidth: '8vw'}}
+                                            onClick={() => setModalOpen(!modalOpen)}
+                                        >
+                                            Go Back
+                                        </button>
+                                    </div>
+                                    <div className="column is-6 has-text-centered">
+                                        <button className="button is-medium" id="so" style={{minWidth: '8vw'}} onClick={signOut}>
+                                            Yes
+                                        </button>
+                                    </div>
+                                </div>
+                            </ModalFooter>
+                            <br/>
+                        </div>
+                    </Modal>
             </>
         )
     }
